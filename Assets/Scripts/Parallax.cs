83516df15f576950a64ea2f9418ea82afda6d48e
Jungyeop Kim <61561973+Jungdol@users.noreply.github.com>
2021-09-01 20:26:46 +0900

    FlyingMonster2 Setting

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,28 +4,72 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
-    public GameObject MainCamera;
+    [Tooltip("메인 카메라 안에 있는 카메라 매니저")]
+    public CameraManager cameraManager;
     public float speed;
-    float tempTr;
-    float timer;
-    float dir;
-    float dir2;
+    [SerializeField]
+    public ParallaxElement[] Elements;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ParallaxElement e in Elements)
+        {
+            e.Start();
+        }
+    }
+
+    // Update is called once per frame
     void FixedUpdate()
     {
-        if (timer >= 0.1f)
+        transform.position = new Vector2(cameraManager.transform.position.x, cameraManager.transform.position.y + 1f);
+        //transform.position += new Vector3(1, 1, 0) * Time.deltaTime;
+        transform.position = Vector2.Lerp(transform.position, cameraManager.transform.position, speed * Time.deltaTime);
+
+
+        foreach (ParallaxElement e in Elements)
         {
-            dir = tempTr - MainCamera.transform.position.x;
-            timer = 0;
+            e.Update();
+        }
+        // 카메라 위치를 따라가는 데 움직일 수록 - 로 해야 뒤로 밀림.
+        // 카메라를 자식으로 두면 흔들리는 스크립트 때 배경도 흔들림.
+    }
+
+    [System.Serializable]
+    public class ParallaxElement
+    {
+        float tempTr;
+        float timer;
+        float dir;
+        float dir2;
+        public Parallax parallax;
+        public GameObject[] GameObjects;
+        [Range(0.0f, 100f)]
+        public float SpeedRatio;
+
+        public void Start()
+        {
+            //Parallax parallax = GetComponent<Parallax>();
         }
 
-        else if (timer >= 0)
+        public void Update()
         {
-            tempTr = MainCamera.transform.position.x;
-            timer += Time.deltaTime;
-        }
-        transform.Translate(new Vector2(dir, 0) * speed * Time.deltaTime);
+            foreach (GameObject obj in GameObjects)
+            {
+                if (timer >= 0.1f)
+                {
+                    dir = tempTr - parallax.cameraManager.transform.position.x;
+                    timer = 0;
+                }
 
-            //transform.position = Vector3.Lerp(MainCamera.transform.position, Time.deltaTime * smoothSpeed);
+                else if (timer >= 0)
+                {
+                    tempTr = parallax.cameraManager.transform.position.x;
+                    timer += Time.deltaTime;
+                }
+
+                obj.transform.Translate( new Vector2 (dir * SpeedRatio * Time.deltaTime, 0.0f));
+            }
+        }
     }
 }
